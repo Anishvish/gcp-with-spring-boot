30fa6f6853554ec43ba7031a86becaf77acf8b9a
anishvish <anishvish5@gmail.com>
2023-09-30 20:11:27 +0530

    initial commit

---
+++ src/main/java/com/spring/cloud/gcp/GcpWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/cloud/gcp/GcpWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.gcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
